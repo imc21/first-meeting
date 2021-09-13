591dd16ff43bd4a14d465385a9e5d727f45d3be9
Caleb Im <calebim@gmail.com>
2021-09-13 09:48:54 -0400

    initial commit -project created

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,15 @@
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+        int x=0;
+        
+        do{
+        
+                x++;
+                System.out.println("i = " + x);
+                
+        
+        }while(x!=10);
+    }
+}
